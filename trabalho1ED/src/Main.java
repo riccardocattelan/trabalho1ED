44566953ae3a6f1a9fb41c71da017d352efba055
riccardocattelan <riccardo.cattelan0@gmail.com>
2025-08-18 20:37:30 -0300

    fazendo de acordo com o enunciado: talvez finalizado

--- trabalho1ED/src/Main.java
+++ trabalho1ED/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -5,23 +6,50 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         PilhaQ1 pilha = new PilhaQ1();
-        pilha.push(1);
-        pilha.push(4);
-        pilha.push(6);
-        int topo=pilha.top();
-        System.out.println(topo);
-        pilha.push(3);
-        pilha.pop();
-        pilha.push(8);
-        topo=pilha.top();
-        System.out.println(topo);
-        boolean vc=pilha.cheia();
-        System.out.println(vc);
-        pilha.pop();
-        pilha.pop();
-        pilha.pop();
-        pilha.pop();
-        vc=pilha.vazia();
-        System.out.println(vc);
+
+        String resposta="s";
+        while(resposta.equals("s")){
+            System.out.printf("%nEscolha qual opção deseja: %n 1 - Inserir %n 2 - Deletar %n 3 - Verificar valor no topo" +
+                    "%n 4 - Verificar se é palíndromo %n");
+            int opcao=scanner.nextInt();
+            scanner.nextLine();
+            switch(opcao){
+                case 1:
+                    pilha.inserir();
+                    break;
+                case 2:
+                    pilha.deletar();
+                    break;
+                case 3:
+                    pilha.top();
+                    break;
+                case 4:
+                    boolean verificar=pilha.verificacao();
+                    System.out.println(verificar);
+                    break;
+            }
+            System.out.printf("%n Deseja continuar? %n Sim - Digite 's' %n Não - Aperte Enter %n");
+            resposta=scanner.nextLine();
+        }
+//        pilha.inserir();
+//        pilha.inserir();
+//        pilha.inserir();
+//        String topo=pilha.top();
+//        System.out.println(topo);
+//        pilha.inserir();
+//        boolean ver=pilha.verificacao();
+//        System.out.println(ver);
+//        pilha.deletar();
+//        pilha.inserir();
+//        topo=pilha.top();
+//        System.out.println(topo);
+//        boolean vaziaoun=pilha.vazia();
+//        System.out.println(vaziaoun);
+//        pilha.deletar();
+//        pilha.deletar();
+//        pilha.deletar();
+//        pilha.deletar();
+//        vaziaoun=pilha.vazia();
+//        System.out.println(vaziaoun);
     }
 }
--- trabalho1ED/src/PilhaQ1.java
+++ trabalho1ED/src/PilhaQ1.java
@@ -1,31 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PilhaQ1 {
-    int[] elementos;
+    ArrayList<String> elementos;
     int topo;
 
-    public PilhaQ1(){
-        elementos=new int[10];
-        topo=-1; //-1 pelo motivo de que para adicionar um item, o topo vai ser topo++
+    public PilhaQ1() {
+        elementos = new ArrayList<>();
+        topo = -1; //-1 pelo motivo de que para adicionar um item, o topo vai ser topo++
         // (adicionado 1 casa decimal) para chegar ao 0, que é o começo, então precisa ser -1 no começo
     }
-    public void push(int e){
+
+    public void inserir() {
+        Scanner scanner = new Scanner(System.in);
         topo++;
-        elementos[topo]=e;
-    }
-    public int pop(){
-        int e;
-        e=elementos[topo];
-        topo--;
-        return e;
-    }
-    public boolean cheia(){
-        return (topo==9); //o número é 9 pois um array de 10 elementos vai do 0-9, sendo 9 o topo
-    }
-    public boolean vazia(){
-        return (topo==-1);
-    }
-    public int top(){
-        return (elementos[topo]);
+        System.out.println("insira um valor");
+        String valor = scanner.nextLine();
+        elementos.add(valor);
     }
 
+    public void deletar() {
+        try {
+            String valor = elementos.remove(topo);
+            topo--;
+            System.out.println("Valor deletado: " + valor);
+        } catch (Exception e) {
+            System.out.println("Erro: Pilha vazia. " + e.getMessage());
+        }
+    }
+
+    public void top() {
+        try {
+            System.out.println("O valor no topo é: "+elementos.get(topo));
+        } catch (Exception e) {
+            System.out.println("Erro: Pilha vazia, não tem valor no topo. " + e.getMessage());
+        }
+    }
+
+    public boolean verificacao() {
+        int cont = 0;
+        if (topo != -1) {
+            while (cont < elementos.size() / 2) {
+                if (!elementos.get(cont).equalsIgnoreCase(elementos.get(elementos.size() - 1 - cont))) {
+                    return false;
+                }
+                cont++;
+            }
+            return true;
+        } else {
+            System.out.println("Pilha vazia! Não tem como fazer essa verificação");
+            return false;
+        }
+    }
 
 }
