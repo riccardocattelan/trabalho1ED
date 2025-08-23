f59c4eeb9b6e609b2b9b69f81137440a7243cb89
riccardocattelan <riccardo.cattelan0@gmail.com>
2025-08-22 22:37:20 -0300

    adicionando comentarios na questao 2

--- trabalho1ED/src/questao2/questao2/GCQ2.java
+++ trabalho1ED/src/questao2/questao2/GCQ2.java
@@ -7,59 +7,59 @@ import java.util.Scanner;
 
 public class GCQ2 {
     static int[] array;
-    static boolean verificarAlocado = false;
+    static boolean verificarAlocado = false;//começa com false pois ainda não foram alocados os blocos de memória
 
     Scanner scanner = new Scanner(System.in);
-    public static void verificacao() {
-        if (verificarAlocado==true) {
+    public static void verificacao() {//método para verificar se os blocos foram alocados
+        if (verificarAlocado==true) {//a verificacao  positiva acontece caso a verificação der true (foi alocado)
             System.out.println("A memória já está alocada");
-        } else {
+        } else {//caso der false (não foram alocados), então vai ser printado o aviso para o usuário
             System.out.println("A memória não está alocada");
         }
     }
 
-    public static void alocar(int tamanho) {
-        if (tamanho>0) {
-            array=new int[tamanho];
-            verificarAlocado=true;
+    public static void alocar(int tamanho) {//é feito o alocamento da memória
+        if (tamanho>0) {//verifica se nao for 0 para nao criar um array de tamanho menor que 0
+            array=new int[tamanho];//criando o array com o tamanho informado
+            verificarAlocado=true;//informação de que foram alocados os blocos (true)
             System.out.printf("Foram alocados %d blocos na memória", tamanho);
         } else {
             System.out.println("Erro: tamanho inválido");
         }
     }
 
-    public static void exibir() {
+    public static void exibir() {//são exibidos os valores que estão salvos na memória
         try {
-            if (verificarAlocado == true && array!=null) {
-                int j = 0;
+            if (verificarAlocado == true && array!=null) {//vai ser feito caso o array nao tiver sido liberado antes e caso os blocos foram alocados
+                int j = 0;//aux para o printf
                 for (int num : array) {
-                    System.out.printf("Index %d: valor %d%n", j, num);
+                    System.out.printf("Index %d: valor %d%n", j, num);//printa os valores junto com o index do array (j)
                     j++;
                 }
-            } else if (array==null) {
+            } else if (array==null) {//caso o array tiver sido liberado anteriormente vai aparecer o aviso para o usuário
                 System.out.println("Array está vazio");
             }
-        } catch (Exception e) {
+        } catch (Exception e) {//tratamento de possíveis erros
             System.out.println(e.getMessage());
         }
     }
 
-    public static void liberarMem() {
-        if (verificarAlocado==true) {
+    public static void liberarMem() {//método para liberar memória
+        if (verificarAlocado==true) {//verifica se ja tinha sido alocado anteriormente, para fazer a liberação
             verificarAlocado = false;
             array = null; //deixando o array sem utilidade, para o Garbage Collector limpar
             System.gc(); //chamando o Garbage Collector só como forma de incentivar o GB a acontecer, mas não é certo,
             // pois o é o JVM que decide quando o Garbage Collector vai acontecer de fato
             System.out.println("A memória foi liberada.");
-        } else if (array==null){
+        } else if (array==null){//verificacao para caso nao tiver blocos de memoria alocados (tiver sido limpo anteriormente)
             System.out.println("A memória já foi liberada antes");
         }
     }
 
-    public static void inserir(int pos, int valor) {
+    public static void inserir(int pos, int valor) {//método para inserir os valores
         try{
-            if (verificarAlocado==true && pos>=0) {
-                array[pos] = valor;
+            if (verificarAlocado==true && pos>=0) {//verifica se os blocos foram alocados e se a posicao for maior ou igual a 0
+                array[pos] = valor;//insere o valor na posicao do array
             }
         } catch (Exception e) {
                 System.out.println(e.getMessage());
--- trabalho1ED/src/questao2/questao2/Main.java
+++ trabalho1ED/src/questao2/questao2/Main.java
@@ -8,36 +8,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        GCQ2 Gc = new GCQ2();
-        System.out.print("Quantos números de blocos deseja alocar? ");
+        GCQ2 Gc = new GCQ2(); //criando objeto da classe
+        System.out.print("Quantos números de blocos deseja alocar? ");// usuario escolhe numero de blocos para alocar
         int nBlocos = scanner.nextInt();
         scanner.nextLine();
-        Gc.alocar(nBlocos);
+        Gc.alocar(nBlocos);//chamando metodo para alocar a quantidade de blocos(ainda sem valores)
 
-        if (Gc.verificarAlocado==true) {
-            for (int i = 0; i < Gc.array.length; i++) {
+        if (Gc.verificarAlocado==true) {//se os blocos foram alocados, entao pode começcar a inserir os valores
+            for (int i = 0; i < Gc.array.length; i++) {//para nao sair do range do array
                 System.out.printf("%nInsira um inteiro para ser o elemento no index %d: ",
                         i);
                 int valorInt= scanner.nextInt();
                 scanner.nextLine();
-                Gc.inserir(i, valorInt);
+                Gc.inserir(i, valorInt);//agora está inserindo os valores nos blocos alocados
             }
             int opcao=0;
             String resposta="s";
             while (resposta.equals("s")) {
                 System.out.printf("%nQual opção deseja executar?%n1 - Exibir array%n2 - " +
-                        "Verificar se está alocado%n3 - Liberar a memória%n");
+                        "Verificar se está alocado%n3 - Liberar a memória%n");//pequeno menu para o usuario escolher a opção
                 opcao = scanner.nextInt();
                 scanner.nextLine();
                 switch (opcao) {
                     case 1:
-                        Gc.exibir();
+                        Gc.exibir();//caso o usuario digitar 1, vai exibir os valores inseridos anteriormente
                         break;
                     case 2:
-                        Gc.verificacao();
+                        Gc.verificacao();//caso digitar 2, vai verificar se os blocos foram alocados
                         break;
                     case 3:
-                        Gc.liberarMem();
+                        Gc.liberarMem();//caso digitar 3, vai ser executada a liberação da memória
                         break;
                 }
                 System.out.printf("%n Deseja continuar? %n Sim - Digite 's' %n Não - Aperte Enter %n");
